39811186f6e1121d9ccc3221de6ded0d2e66e247
shawn_shaw <cola_ocean@foxmail.com>
2025-05-09 14:10:37 +0800

    blocks
    Signed-off-by: shawn_shaw <cola_ocean@foxmail.com>

--- database/blocks.go
+++ database/blocks.go
@@ -1,1 +1,13 @@
 package database
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"math/big"
+)
+
+type Blocks struct {
+	Hash       common.Hash `gorm:"primaryKey;serializer:bytes"`
+	ParentHash common.Hash `gorm:"serializer:bytes"`
+	Number     *big.Int    `gorm:"serializer:u256"`
+	Timestamp  uint64
+}
